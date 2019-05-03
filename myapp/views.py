263ea8a189d875af9002603a43dd446c8089f11c
khjbest <rnjsguswl393@likelion.org>
2019-05-03 21:09:25 +0900

    blog

--- myapp/views.py
+++ myapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Blog
 # Create your views here.
 def home(request):
-    blogs=Blog.objects
+    blogs=Blog.objects.all().order_by('-id')
     return render(request, 'home.html',{'blogs':blogs})
